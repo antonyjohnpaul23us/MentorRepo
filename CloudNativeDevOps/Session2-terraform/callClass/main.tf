b74db89c479edbdd62b1e5cb15643cf3a26e5372
Antony John Paul <john.paul@omnicell.com>
2021-12-13 09:00:59 -0500

    Updating code repo

--- CloudNativeDevOps/Session2-terraform/callClass/main.tf
+++ CloudNativeDevOps/Session2-terraform/callClass/main.tf
@@ -1,1 +1,3 @@
-
+module "ec2_creation" {
+  source  = "../tf_modules/ec2"
+}
---
+++ ECS_Lambda_RayTrackingExample/ecs-worker-launcher/config.js
@@ -0,0 +1,5 @@
+{
+    "queue": "https://<-REGION>.queue.amazonaws.com/<YOUR-AWS-ACCOUNT-ID>/ECSPOVRayWorkerQueue",
+    "task": "ECSPOVRayWorkerTask",
+    "s3_key_suffix_whitelist": [".zip"]
+}
